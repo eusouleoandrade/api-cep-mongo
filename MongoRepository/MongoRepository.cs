33ee8de2c30c420d746861970ef4bf04905ead3c
Leandro Rafael de Andrade Machado <l.de.andrade.machado@avanade.com>
2020-05-24 04:00:34 -0300

    Created init and constructor CosmoDB Azure

--- MongoRepository/MongoRepository.cs
+++ MongoRepository/MongoRepository.cs
@@ -1,16 +1,25 @@
 ﻿using MongoDB.Driver;
+using System.Security.Authentication;
 
 namespace MongoRepository
 {
     public abstract class MongoRepository
     {
-        private readonly string _connectionString = Properties.Settings.Default.connectionStringApiCepDB;
+        private readonly string _connectionString = Properties.Settings.Default.connectionStringApiCepDBAzure;
         protected IMongoClient _client;
         protected IMongoDatabase _dataBase;
 
         protected MongoRepository()
         {
-            _client = new MongoClient(_connectionString);
+            // Default connection
+            //_client = new MongoClient(_connectionString);
+            //_dataBase = _client.GetDatabase("apicepdb");
+
+            // SSL connection
+            MongoClientSettings settings = MongoClientSettings.FromUrl(new MongoUrl(_connectionString));
+            settings.SslSettings = new SslSettings() { EnabledSslProtocols = SslProtocols.Tls12 };
+
+            _client = new MongoClient(settings);
             _dataBase = _client.GetDatabase("apicepdb");
         }
     }
