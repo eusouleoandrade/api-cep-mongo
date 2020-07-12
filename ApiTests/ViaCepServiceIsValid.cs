db75f7925b016087aa7567a4824b36858556e6a7
Leandro Rafael de Andrade Machado <l.de.andrade.machado@avanade.com>
2020-07-12 01:25:20 -0300

    Inserting unit test for method IsValid at ViaCepService.

---
+++ ApiTests/ViaCepServiceIsValid.cs
@@ -0,0 +1,36 @@
+﻿using cepService;
+using cepService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApiTests
+{
+    public class ViaCepServiceIsValid
+    {
+        private ICepService _cepService;
+
+        [Theory]
+        [InlineData(false, "530808000")]
+        [InlineData(false, "53080")]
+        [InlineData(false, "53080A00")]
+        [InlineData(true, "53080800")]
+        [InlineData(true, "52041550")]
+        [InlineData(true, "53150600")]
+        public void CheckIfCepIsValidAndNotValid(bool isValid, string cep)
+        {
+            // Arranje
+            _cepService = new ViaCepService();
+
+            // Act
+            var result = _cepService.IsValid(cep);
+
+            // Assert
+            if (isValid)
+                Assert.True(result);
+            else
+                Assert.False(result);
+        }
+    }
+}
