29b993cc8c2b5c29527bc88bc07a32b160280988
Leandro Rafael de Andrade Machado <l.de.andrade.machado@avanade.com>
2020-07-21 01:46:50 -0300

    Creating unit test for Mongo Repository.

---
+++ ApiTests/MongoRepositoryExceptionCtor.cs
@@ -0,0 +1,65 @@
+﻿using Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ApiTests
+{
+    public class MongoRepositoryExceptionCtor
+    {
+        private MongoRepositoryException _exception;
+        private string _message = "Failed to connect the database.";
+
+        [Fact]
+        public void CheckCreationObject()
+        {
+            // Arranje / Act
+            _exception = new MongoRepositoryException();
+
+            // Assert
+            Assert.NotNull(_exception);
+        }
+
+        [Fact]
+        public void CheckCreationObjectWithMessage()
+        {
+            // Arranje / Act
+            _exception = new MongoRepositoryException(_message);
+
+            // Assert
+            Assert.NotNull(_exception);
+            Assert.Equal(_message, _exception.Message);
+        }
+
+        [Fact]
+        public void CheckCreationObjectWithMessageAndInnerException()
+        {
+            // Arranje
+            var innerException = new Exception();
+
+            // Act
+            _exception = new MongoRepositoryException(_message, innerException);
+
+            // Assert
+            Assert.NotNull(_exception);
+            Assert.NotNull(_exception.InnerException);
+            Assert.Equal(_message, _exception.Message);
+        }
+
+        [Fact]
+        public void CheckCreationObjectWithInnerException()
+        {
+            // Arranje
+            var innerException = new Exception();
+
+            // Act
+            _exception = new MongoRepositoryException(innerException);
+
+            // Assert
+            Assert.NotNull(_exception);
+            Assert.NotNull(_exception.InnerException);
+            Assert.Equal(_message, _exception.Message);
+        }
+    }
+}
